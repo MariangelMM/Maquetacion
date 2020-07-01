f4ec4f865653106483567b0ce27cc58a40cb4cb3
MariangelMM <mariangelmoramarquez@gmail.com>
2020-07-01 16:49:29 -0500

    Updates

---
+++ precache-manifest.c8c28087777490495eb3b2f4b08504ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b776f4af23e31b59cc98c0aea03a402",
+    "url": "/Maquetacion/index.html"
+  },
+  {
+    "revision": "1406095a39aad986cc32",
+    "url": "/Maquetacion/static/css/main.0ab03ce7.chunk.css"
+  },
+  {
+    "revision": "43a30bbfa578caa6335c",
+    "url": "/Maquetacion/static/js/2.71a1f868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Maquetacion/static/js/2.71a1f868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1406095a39aad986cc32",
+    "url": "/Maquetacion/static/js/main.6186b793.chunk.js"
+  },
+  {
+    "revision": "ba9c4c976112bf86104e",
+    "url": "/Maquetacion/static/js/runtime-main.055dba85.js"
+  },
+  {
+    "revision": "1823f534afc8e9a5ad3413441d48b819",
+    "url": "/Maquetacion/static/media/Dexter.1823f534.png"
+  },
+  {
+    "revision": "3839a6b0de73385f6d5ae09d589dbd71",
+    "url": "/Maquetacion/static/media/Didi.3839a6b0.png"
+  },
+  {
+    "revision": "7aa4858da571f69d236e38e3620641eb",
+    "url": "/Maquetacion/static/media/Mandark.7aa4858d.png"
+  }
+]);
